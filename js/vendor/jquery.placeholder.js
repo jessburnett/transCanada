5c1caf336b38ad096afebabae46d77bf9b9f2f9a
Jess <jess.a.burnett@gmail.com>
2014-10-06 13:46:05 -0500

    latest dev

---
+++ js/vendor/jquery.placeholder.js
@@ -0,0 +1,16 @@
+if (!$.support.placeholder) {
+
+            var active = document.activeElement;
+            $(':text, textarea').focus(function () {
+                if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+                    $(this).val('').removeClass('hasPlaceholder');
+                }
+            }).blur(function () {
+                if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+                    $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+                }
+            });
+            $(':text, textarea').blur();
+            $(active).focus();
+
+        }
